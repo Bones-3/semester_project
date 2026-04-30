5c31050c850bca8765dc47eb6fc124cc3cf157fe
Fabián Jara <fa.jarao@duocuc.cl>
2026-04-30 00:31:27 -0400

    Estructura inicial de MS-Autentificacion, Productos y Usuarios

---
+++ ms_autentificacion/src/test/java/com/restaurant/ms_autentificacion/MsAutentificacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restaurant.ms_autentificacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAutentificacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
